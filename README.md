e815d6b10ecf8993abe52134231ca187c433625a
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-09-26 18:30:59 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Create env
+
+```bash
+conda create -n winequality_mlops python=3.10 -y
+```
+
+Activate Environment
+```bash
+conda activate winequality_mlops
+```
+
+Create `requirements.txt` file
+```bash
+touch requirements.txt
+```
+
+Create `README.md` file
+```bash
+touch README.md
+```
+Install requriments
+```bash
+pip install -r requirmnets.txt
+```
+
---
+++ template.py
@@ -0,0 +1,26 @@
+import os
+
+dirs = [
+    os.path.join('data','raw'),
+    os.path.join('data','processed'),
+    os.path.join('data','raw'),
+    'notebooks',
+    'data_given',
+    'saved_models',
+    'src'
+]
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok=True)
+    with open(os.path.join(dir_, '.gitignore'), 'w') as f:
+        pass
+
+files = [
+    'dvc.yaml',
+    'params.yaml',
+    '.gitignore',
+    os.path.join('src', '__init__.py')
+]
+
+for file_ in files:
+    with open(file_, 'w') as f:
+        pass
