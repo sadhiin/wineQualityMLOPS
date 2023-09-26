717934235e8b27ab04d30595c21d808329a15cf2
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-09-26 20:44:55 +0600

    Added and updated dvc stage-2: split data

---
+++ src/split_data.py
@@ -0,0 +1,34 @@
+# split the raw data
+# save it data/processed
+
+import os
+import argparse
+import pandas as pd
+from get_data import read_params, get_data
+from sklearn.model_selection import train_test_split
+
+
+def split_and_save_data(config_path):
+    config = read_params(config_path)
+    train_data_path = config["split_data"]["train_path"]
+    test_data_path = config["split_data"]["test_path"]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    split_ratio = config["split_data"]["test_size"]
+    random_state = config["base"]["random_state"]
+
+    df = pd.read_csv(raw_data_path, sep=',', encoding='utf-8')
+    train, test = train_test_split(
+        df,
+        test_size= split_ratio,
+        random_state= random_state)
+
+    train.to_csv(train_data_path, sep=',', index=False, encoding='utf-8')
+    test.to_csv(test_data_path,  sep=',', index=False, encoding='utf-8')
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+
+    parsed_args = args.parse_args()
+
+    split_and_save_data(config_path= parsed_args.config)
