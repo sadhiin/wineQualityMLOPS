a3c6de2cea3bd5e1412c90a492b22ccd8f9555fe
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-09-28 00:27:21 +0600

    py-test eample added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,16 @@
-
+import pytest
+class NotInRangeError(Exception):
+    def __init__(self, message="Error message"):
+        self.message = message
+        super().__init__(self.message)
 
 def test_generaic():
     a = 3
     b = 3
     assert a == b
+
+def test_withexception():
+    a = 15  # Change the value of 'a' to be within the range (10, 20)
+    with pytest.raises(NotInRangeError):
+        if a in range(10, 20):
+            raise NotInRangeError("Value is not in range")
