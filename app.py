45af2b3c94d1de432bafeae5b92d85d1c427d1cf
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-09-29 02:13:10 +0600

    web app and API prediction is done

--- app.py
+++ app.py
@@ -12,10 +12,47 @@ template_dir = os.path.join(webapp_root, 'templates')
 
 app = Flask(__name__ , static_folder= static_dir, template_folder= template_dir)
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def prediction(data):
+    config = read_params(param_path)
+    model_dir = config["webapp_model_dir"]
+    model = joblib.load(model_dir)
+    pred = model.predict(data)
+    # print(pred)
+    return pred[0]
+
+def api_responce(api_input):
+    try:
+        data = np.array([list(api_input.json.values())])
+        response = prediction(data)
+        response = {"response": response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong! Please try again."}
+        return error
+
+
 @app.route("/", methods= ['GET', 'POST'])
 def index():
     if request.method == "POST":
-        pass
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float, data))]
+                response = prediction(data)
+                return render_template("index.html", response= response)
+            elif request.json:
+                responce = api_responce(request)
+                return jsonify(responce)
+        except Exception as e:
+            print(e)
+            error ={"error": "Something went wrong! Please, Try again."}
+            return render_template('404.html', error = error)
     else:
         return render_template("index.html")
 
