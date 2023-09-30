e43403a214d97edbefd22115fe44792a0b5b1c9b
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-09-30 13:29:46 +0600

    updated test cases

--- tests/test_config.py
+++ tests/test_config.py
@@ -64,3 +64,12 @@ def test_api_rsponse_expected_range(data = input_data['correct_range']):
     res = api_response(data)
     assert TARGET_range['min'] <= res['response'] <= TARGET_range['max']
 
+
+
+def test_form_rsponse_unexpected_range(data = input_data['incorrect_range']):
+    with pytest.raises(prediction_service.prediction.NotInRange):
+        res = form_response(data)
+
+def test_api_rsponse_unexpected_range(data=input_data['incorrect_range']):
+    res = api_response(data)
+    assert res['response'] == prediction_service.prediction.NotInRange().message
